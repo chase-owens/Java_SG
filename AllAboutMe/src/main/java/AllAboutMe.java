4dec82ce74539083ab923e7cbcfe01f92ae8bfad
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-30 17:00:54 -0500

    CHOWINDWRK-8 All About Me

---
+++ AllAboutMe/src/main/java/AllAboutMe.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chaseowens
+ */
+public class AllAboutMe {
+    public static void main(String[] args) {
+        
+        String name, favoriteFood;
+        int numberOfPets, ageILearnedToWhistle;
+        boolean eatenGnocchi;
+        
+        name = "Chase Owens";
+        favoriteFood = "steak";
+        numberOfPets = 2;
+        ageILearnedToWhistle = 4;
+        eatenGnocchi = true;
+        
+        System.out.println("I am " + name);
+        System.out.println("I have " + numberOfPets + " pets");
+        System.out.println("My favorite food is " + favoriteFood);
+        System.out.println("It is " + eatenGnocchi + " that I have eaten gnocchi.");
+        System.out.println("And when I was " + ageILearnedToWhistle + "years old, I learned to whistle.");
+        
+    }
+}
