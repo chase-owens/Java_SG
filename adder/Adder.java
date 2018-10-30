938aa5233e585918336ce811a26ea71159a307fe
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-30 14:00:49 -0500

    Adder Java

---
+++ adder/Adder.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Adder {
+    public static void main(String[] args) {
+        // Declare varaibles to use
+        int sum = 0;
+        int operand1 = 0;
+        int operand2 = 0;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        // Initialize the operands
+
+        String stringOperand1 = "";
+        String stringOperand2 = "";
+
+        System.out.println("Please enter the first number to be added:");
+        stringOperand1 = myScanner.nextLine();
+
+        System.out.println("Please enter the second number to be added:");
+        stringOperand2 = myScanner.nextLine();
+
+        operand1 = Integer.parseInt(stringOperand1);
+        operand2 = Integer.parseInt(stringOperand2);
+
+        sum = operand1 + operand2;
+
+        System.out.println("Sum is: " + sum);
+        myScanner.close();
+    }
+}
