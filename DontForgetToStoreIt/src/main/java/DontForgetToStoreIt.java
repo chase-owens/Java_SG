5f10d07053cd048fd9b2d8ea39c69b20188f3755
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-31 10:50:51 -0500

    closeScanners

--- DontForgetToStoreIt/src/main/java/DontForgetToStoreIt.java
+++ DontForgetToStoreIt/src/main/java/DontForgetToStoreIt.java
@@ -17,22 +17,24 @@ public class DontForgetToStoreIt {
         final double PI = 3.14159;
         double pi;
         String cheese, color;
-        
+
         Scanner inputReader = new Scanner(System.in);
-        
+
         System.out.println("Give me pi to at least 5 decimals: ");
         pi = inputReader.nextDouble();
-        
+
         System.out.println("What is the meaning of life, the universe and everything? ");
         meaningOfLifeAndEverything = inputReader.nextInt();
-        
+
         System.out.println("What is your favorite kind of cheese? ");
         cheese = inputReader.next();
-        
+
         System.out.println("Do you like the color red or blue more? ");
         color = inputReader.next();
-        
+
         System.out.println("Ooh, " + color + " " + cheese + " sounds delicous!");
         System.out.println("The circumference of life is " + (2 * pi * meaningOfLifeAndEverything));
+
+        inputReader.close();
     }
 }
--- PassingTheTuringTest/src/main/java/PassingTheTuringTest.java
+++ PassingTheTuringTest/src/main/java/PassingTheTuringTest.java
@@ -14,35 +14,38 @@ import java.util.Scanner;
 public class PassingTheTuringTest {
     public static void main(String[] args) {
         Scanner inputReader = new Scanner(System.in);
-        
+
         String name, myName, favoriteColor, myFavoriteColor, favoriteFood, myFavoriteFood;
         double favoriteNumber, myFavoriteNumber, sum;
-        
+
         myName = "Zeph";
         myFavoriteColor = "green";
         myFavoriteFood = "byte";
-        
+
         myFavoriteNumber = 42;
-        
+
         System.out.println("Hi there! Thanks for visiting me. What is your name");
         name = inputReader.nextLine();
         System.out.println("Nice to meet you " + name + ". My name is " + myName + ".");
-        
+
         System.out.println("What's your favorite color? ");
         favoriteColor = inputReader.nextLine();
         System.out.println(favoriteColor + "?? Cool, my favorite color is " + myFavoriteColor);
-        
+
         System.out.println("What about your favorite food? ");
         favoriteFood = inputReader.nextLine();
-        System.out.println(favoriteFood + " sounds really good! I could take a " + myFavoriteFood + " out of anything!!");
-        
+        System.out
+                .println(favoriteFood + " sounds really good! I could take a " + myFavoriteFood + " out of anything!!");
+
         System.out.println("Tell me your favorite number. ");
         favoriteNumber = inputReader.nextDouble();
         sum = favoriteNumber + myFavoriteNumber;
-        System.out.println(favoriteNumber + " is a cool number! My favorite number is " + myFavoriteNumber + ". " + favoriteNumber + " + " + myFavoriteNumber + " is " + sum + "! That's a cool number too!! What a combination we make!");
-        
+        System.out.println(favoriteNumber + " is a cool number! My favorite number is " + myFavoriteNumber + ". "
+                + favoriteNumber + " + " + myFavoriteNumber + " is " + sum
+                + "! That's a cool number too!! What a combination we make!");
+
         System.out.println("Well, thanks for talking to me " + name + ".");
-        
-        
+
+        inputReader.close();
     }
 }
