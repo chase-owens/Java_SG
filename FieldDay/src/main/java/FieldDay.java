6949b3f6db9c3f4ac40bb1f19605e3078efad57f
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-31 16:44:30 -0500

    CHOWINDWRK-25 FieldDay

---
+++ FieldDay/src/main/java/FieldDay.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chaseowens
+ */
+import java.util.Scanner;
+
+public class FieldDay {
+
+    public static void main(String[] args) {
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("What's your last name? ");
+        String name = userInput.next();
+        
+        if (name.compareTo("Baggins") < 0) {
+            System.out.println("You're on the \"Red Dragons\"");
+        } else if (name.compareTo("Dresden") < 0) {
+            System.out.println("You're on the \"Dark Wizards\"");
+        } else if (name.compareTo("Howl") < 0) {
+            System.out.println("You're on the \"Moving Castles\"");
+        } else if (name.compareTo("Potter") < 0) {
+            System.out.println("You're on the \"Golden Snitches\"");
+        } else if (name.compareTo("Vimes") < 0) {
+            System.out.println("You're on the \"Night Guards\"");
+        } else {
+            System.out.println("You're on the \"Black Holes\"");
+        }
+        
+        userInput.close();
+    }
+
+}
