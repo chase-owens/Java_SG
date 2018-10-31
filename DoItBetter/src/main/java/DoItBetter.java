df0639e7c611826d0d65308a7500ed6247c8a7ba
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-31 11:05:26 -0500

    CHOWINDWRK-15 oneUpper

--- DoItBetter/src/main/java/DoItBetter.java
+++ DoItBetter/src/main/java/DoItBetter.java
@@ -8,6 +8,28 @@
  *
  * @author chaseowens
  */
+
+import java.util.Scanner;
+
 public class DoItBetter {
-    
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        
+        double milesCanRun, hotdogsCanEat, languagesKnown;
+        
+        System.out.println("How many miles can you run? ");
+        milesCanRun = inputReader.nextDouble();
+        double milesICanRun = milesCanRun + 1;
+        System.out.println("Nice, while you can run " + milesCanRun + " miles. I can run " + milesICanRun + " miles!");
+        
+        System.out.println("How many hot dogs can you eat? ");
+        hotdogsCanEat = inputReader.nextDouble();
+        double hotdogsICanEat = hotdogsCanEat + 1;
+        System.out.println(hotdogsCanEat + "? Well you're no Joey Chestnut... I can eat " + hotdogsICanEat + " dogs.");
+        
+        System.out.println("How many languages do you know? ");
+        languagesKnown = inputReader.nextDouble();
+        double languagesIKnow = languagesKnown + 1;
+        System.out.println(languagesKnown + "?! That's how many I knew when I was 15!! I know " + languagesIKnow + " languages!");
+    }
 }
