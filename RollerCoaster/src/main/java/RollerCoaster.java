1a7b75333ed1b8c58bb2d44d90a7b0e1d4760207
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 10:30:18 -0500

    CHOWINDWRK-39 RollerCoaster

--- RollerCoaster/src/main/java/RollerCoaster.java
+++ RollerCoaster/src/main/java/RollerCoaster.java
@@ -8,6 +8,30 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+
 public class RollerCoaster {
-    
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("We're going to go on a roller coaster");
+        System.out.println("Let me know when you want to get off...!");
+
+        String keepRiding = "y";
+        int loopsLooped = 0;
+
+        while (keepRiding.equals("y")) {
+            System.out.println("WheeeeeeEEEEEEEeeEEEEEeEEe");
+            System.out.println("Want to keep going? (y/n)");
+
+            keepRiding = userInput.next();
+            loopsLooped++;
+
+        }
+        
+        System.out.println("Wow that was FUN!");
+        System.out.println("We looped that loop " + loopsLooped + " times!!");
+
+    }
 }
