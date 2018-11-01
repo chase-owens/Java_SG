f082ddf488d75fb752b0cce67cb0ba22bfc2c07c
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 11:50:20 -0500

    CHOWINDWRK-33 LazyBryson

--- LazyTeenager/src/main/java/LazyTeenager.java
+++ LazyTeenager/src/main/java/LazyTeenager.java
@@ -8,6 +8,29 @@
  *
  * @author chaseowens
  */
+import java.util.Random;
 public class LazyTeenager {
-    
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        int timesToldToCleanRoom = 0;
+        int liklihoodOfCleaningRoom = 0;
+        boolean roomCleaned = false;
+        
+        do {
+            int chance = randomizer.nextInt(100);
+            System.out.println("Clean your room Bryson");
+            liklihoodOfCleaningRoom += 5;
+            timesToldToCleanRoom += 1;
+            
+            if (chance <= liklihoodOfCleaningRoom) {
+                System.out.println("FINE I'LL CLEAN MY ROOM. BUT I REFUSE TO EAT MY PEAS");
+                roomCleaned = true;
+            }
+            
+            if (timesToldToCleanRoom == 15) {
+                System.out.println("You're grounded");
+                break;
+            }
+        } while (roomCleaned == false);
+    }
 }
