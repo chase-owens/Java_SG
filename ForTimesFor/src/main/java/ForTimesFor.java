35760eeef0eaab607512caf62fd7aa7ad30b99b2
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 13:33:50 -0500

    CHOWINDWRK-42 forTimesfor

--- ForTimesFor/src/main/java/ForTimesFor.java
+++ ForTimesFor/src/main/java/ForTimesFor.java
@@ -8,6 +8,26 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+
 public class ForTimesFor {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Which times table shall we look at?");
+        int num = input.nextInt();
+        
+        for (int i = 1; i < 16; i++) {
+            int sum = num * i;
+            System.out.println(i + " * " + num + " is:");
+            int bestGuess = input.nextInt();
+            
+            if (sum == bestGuess) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Sorry no, the answer is " + sum);
+            }
+        }
+    }
 }
