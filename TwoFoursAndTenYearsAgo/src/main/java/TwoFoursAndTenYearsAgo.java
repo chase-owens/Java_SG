79ddbadab8f5efe8482eb1f132f1f0be27835216
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 13:56:16 -0500

    CHOWINDWRK-42 TenYearsBack

--- TwoFoursAndTenYearsAgo/src/main/java/TwoFoursAndTenYearsAgo.java
+++ TwoFoursAndTenYearsAgo/src/main/java/TwoFoursAndTenYearsAgo.java
@@ -8,6 +8,23 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+
 public class TwoFoursAndTenYearsAgo {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("What year would you like to count back from? ");
+        int year = input.nextInt();
+        
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + " years ago would be " + (year - i));
+        }
+        
+        System.out.println("\nI can count backward using a different way too...");
+        
+        for (int i = year; i >= year - 10; i--) {
+            System.out.println((year - i) + " years ago would be " + i);
+        }
+    }
 }
