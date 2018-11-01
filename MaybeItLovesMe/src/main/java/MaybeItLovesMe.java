67454aef2bbefbfe8b23c7f9e8f03fdd0d9afa2f
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 10:55:06 -0500

    CHOWINDWRK-36 MaybeItLovesMe

--- MaybeItLovesMe/src/main/java/MaybeItLovesMe.java
+++ MaybeItLovesMe/src/main/java/MaybeItLovesMe.java
@@ -8,6 +8,30 @@
  *
  * @author chaseowens
  */
+
+import java.util.Random;
 public class MaybeItLovesMe {
-    
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        
+        int numberOfPetals = randomizer.nextInt(76) + 13;
+        boolean lovesMe;
+        
+        while (numberOfPetals > 0) {
+            if (numberOfPetals % 2 == 1) {
+                System.out.println("It loves me!");
+                lovesMe = true;
+            } else {
+                System.out.println("It loves me not!");
+                lovesMe = false;
+            }
+            numberOfPetals--;
+        }
+        if (lovesMe = true) {
+            System.out.println("It really LOVES me!");
+        } else {
+            System.out.println("Awwwww, bummer");
+        }
+        
+    }
 }
