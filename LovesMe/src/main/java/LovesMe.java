ef6ebfc2086b248db9dbb62bfbc5e5f267c593eb
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 10:48:40 -0500

    CHOWINDWRK-37 LoveIt

--- LovesMe/src/main/java/LovesMe.java
+++ LovesMe/src/main/java/LovesMe.java
@@ -9,5 +9,17 @@
  * @author chaseowens
  */
 public class LovesMe {
-    
+    public static void main(String[] args) {
+        int numberOfPetals = 34;
+        
+        while (numberOfPetals > 0) {
+            if (numberOfPetals % 2 == 1) {
+                System.out.println("It LOVES me!");
+            } else {
+                System.out.println("It LOVES me NOT!");
+            }
+            numberOfPetals--;
+        }
+        System.out.println("Treat IT right!");
+    }
 }
