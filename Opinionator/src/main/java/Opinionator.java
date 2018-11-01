449c8db2ce233bbdb383bbc136ef8d924e62309c
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-31 21:10:15 -0500

    CHOWINDWRK-28 Opinioator

--- Opinionator/src/main/java/Opinionator.java
+++ Opinionator/src/main/java/Opinionator.java
@@ -8,8 +8,34 @@
  *
  * @author chaseowens
  */
+import java.util.Random;
+
 public class Opinionator {
+
     public static void main(String[] args) {
+        Random randomizer = new Random();
+
+        System.out.println("I can't decide what animal I like the best.");
+        System.out.println("I know! Random can decide FOR ME!");
+
+        int x = randomizer.nextInt(5);
+
+        System.out.println("The number selected was: " + x);
+
+        if (x == 0) {
+            System.out.println("Llamas are the best!");
+        } else if (x == 1) {
+            System.out.println("Dodos are the best!");
+        } else if (x == 2) {
+            System.out.println("Wooly Mammoths are DEFINITELY the best!");
+        } else if (x == 3) {
+            System.out.println("Sharks are the greatest, they have their own week!");
+        } else if (x == 4) {
+            System.out.println("Cockatoos are just so awesome!");
+        } else if (x == 5) {
+            System.out.println("Have you ever met a Mole-Rat? They're GREAT!");
+        }
         
+        System.out.println("Thanks Random, maybe YOU're the best!");
     }
-}
+} //There was no bug in the code
