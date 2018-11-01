04b0b117ada9538789323be5c59945fca1f08d24
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 11:09:47 -0500

    CHOWINDWRK-35 useSplitMethodStrategy

--- GuessMeFinally/src/main/java/GuessMeFinally.java
+++ GuessMeFinally/src/main/java/GuessMeFinally.java
@@ -8,6 +8,37 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+import java.util.Random;
+
 public class GuessMeFinally {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random randomizer = new Random();
+        
+        int hiddenNumber = randomizer.nextInt(200) - 100;
+        
+        System.out.println("I've chosen a number between -100 and 100. Betcha can't guess it!: ");
+        int numberGuessed = input.nextInt();
+        if (numberGuessed == hiddenNumber) {
+            System.out.println("WOW, Nice guess!");
+            System.out.println("The number I was thinking of was " + hiddenNumber);
+        }
+        
+        while (numberGuessed != hiddenNumber) {
+            if (numberGuessed < hiddenNumber) {
+                System.out.println("Too low, try again!:");
+                numberGuessed = input.nextInt();
+            } else if (numberGuessed > hiddenNumber) {
+                System.out.println("Too high, try again!:");
+                numberGuessed = input.nextInt();
+            }
+        }
+        
+        if (numberGuessed == hiddenNumber) {
+            System.out.println("Finally, it's about time you got it!");
+        }
+        
+    }
 }
