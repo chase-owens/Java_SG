75a04970a11f12537013f8471453770e41488cba
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 10:43:35 -0500

    CHOWINDWRK-38 ToDoOrNotToDo

--- DoOrDoNot/src/main/java/DoOrDoNot.java
+++ DoOrDoNot/src/main/java/DoOrDoNot.java
@@ -8,6 +8,36 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+
 public class DoOrDoNot {
-    
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Should I do it? (y/n)");
+        
+        boolean doIt;
+        
+        if (input.next().equals("y")) {
+            doIt = true;
+        } else {
+            doIt = false;
+        }
+        
+        boolean iDidIt = false;
+        
+        do {
+            iDidIt = true;
+            break;
+        } while (doIt);
+        
+        if (doIt && iDidIt) {
+            System.out.println("I did it!");
+        } else if (!doIt && iDidIt) {
+            System.out.println("I know you said not to ... but I totally did anyways.");
+        } else {
+            System.out.println("Dono't look at me, I didn't do anything!");
+        }
+    }
 }
