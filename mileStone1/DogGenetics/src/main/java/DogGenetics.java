e2cfe5a6a6a76430faa30d7323efad604ff4bc8a
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-02 15:08:16 -0500

    CHOWINDWRK-48 InitCommit

---
+++ mileStone1/DogGenetics/src/main/java/DogGenetics.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chaseowens
+ */
+import java.util.Scanner;
+
+public class DogGenetics {
+    public static void main(String[] args) {
+        Scanner capture = new Scanner(System.in);
+        
+        
+    }
+}
