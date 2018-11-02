8b6f8c3986acd02bbac454222240339bb49c9ed1
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 23:03:00 -0500

    ForByFor

--- ForByFor/src/main/java/ForByFor.java
+++ ForByFor/src/main/java/ForByFor.java
@@ -9,5 +9,29 @@
  * @author chaseowens
  */
 public class ForByFor {
-    
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (i == 1 && j !=1) {
+                        System.out.print("@");
+                    } else if (i == 1 && j == 1){
+                        System.out.print("#");
+                    } else if ((i == 0 || i == 2) && j == 1) {
+                        System.out.print("$");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+                System.out.print("|");
+            }
+            System.out.println("");
+            {
+
+            }
+        }
+    }
 }
