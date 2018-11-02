4ff99b4635e481273c259d008c153277dff3d927
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-02 16:10:39 -0500

    CHOWINDWRK-48 RPS

--- mileStone1/RPS/src/main/java/RPS.java
+++ mileStone1/RPS/src/main/java/RPS.java
@@ -8,6 +8,81 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+import java.util.Random;
+
 public class RPS {
-    
+
+    public static void main(String[] args) {
+        Scanner capture = new Scanner(System.in);
+        Random randomizer = new Random();
+        boolean hasEnteredValidNumber = false, keepPlaying = true;
+        int rounds = 11, tie = 0, userWins = 0, computerWins = 0, compNumber;
+        String userPick, computerPick;
+
+        do {
+
+            // Check to make sure number b/t 1-10 was entered
+            do {
+                System.out.println("Let's play Rock Paper Siccors!!");
+                System.out.println("How many rounds do you want to play? Please only select something between 1 and 10");
+                if (capture.hasNextInt()) {
+                    rounds = capture.nextInt();
+                    if (rounds >= 0 && rounds <= 10) {
+                        hasEnteredValidNumber = true;
+                    }
+                } else {
+                    System.out.println("That is not a number b/t 1 & 10!");
+                    capture.next();
+                }
+
+            } while (!hasEnteredValidNumber);
+            
+            for (int i = 1; i < (rounds + 1); i++) {
+                System.out.println("Pick: rock, paper, or scissors");
+                userPick = capture.next().toLowerCase();
+                System.out.print("Round " + i + "    You picked " + userPick);
+                
+                compNumber = randomizer.nextInt(3);
+                if (compNumber == 0) {
+                    computerPick = "rock";
+                } else if (compNumber == 1) {
+                    computerPick = "paper";
+                } else {
+                    computerPick = "scissors";
+                }
+                System.out.println("    The computer picks " + computerPick);
+                
+                if ((userPick.equals("rock") && computerPick.equals("scissors")) || (userPick.equals("paper") && computerPick.equals("rock")) || (userPick.equals("scissors") && computerPick.equals("paper"))) {
+                    userWins += 1;
+                    System.out.println("You Win!");
+                } else if (userPick.equals(computerPick)) {
+                    tie += 1;
+                    System.out.println("Tie!");
+                } else {
+                    computerWins += 1;
+                    System.out.println("Sorry, you loose");
+                }
+            }
+            
+            System.out.println("User wins: " + userWins + "Computer wins: " + computerWins + "Ties: " + tie);
+            if (userWins > computerWins) {
+                System.out.println("You win!!");
+            } else if (userWins == computerWins) {
+                System.out.println("Kiss your sister, It's a tie");
+            } else {
+                System.out.println("You lose..");
+            }
+            
+            System.out.println("Keep Playing? (y/n)");
+            String play = capture.next();
+            
+            if (play.equals("n")) {
+                keepPlaying = false;
+            }
+
+        } while (keepPlaying == true);
+
+    }
+
 }
