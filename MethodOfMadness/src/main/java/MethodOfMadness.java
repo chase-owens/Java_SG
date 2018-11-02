37e17aa0445b7fdd808377ca4d17476e7ee1a9f4
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-02 08:41:47 -0500

    CHOWINDWRK-47 MethodOfMadness

--- MethodOfMadness/src/main/java/MethodOfMadness.java
+++ MethodOfMadness/src/main/java/MethodOfMadness.java
@@ -10,6 +10,18 @@
  */
 public class MethodOfMadness {
     public static void main(String[] args) {
+        eatMe();
         
+        System.out.println("\n - Lewis Carroll, Alice in Wonderland");
+    }
+    static void eatMe() {
+        System.out.println(" 'But I don't want to go among mad people, ' Alice remarked.");
+        System.out.println(" 'Oh, you can't help that,' said the Cat.");
+        System.out.println(" 'We're all mad here. I'm mad. You're mad.' ");
+    }
+    
+    static void drinkMe() {
+        System.out.println(" 'How do you know I'm mad. You're mad.'");
+        System.out.println(" 'You must be,' said the Cat, 'or you wouldn't have come here.' ");
     }
 }
