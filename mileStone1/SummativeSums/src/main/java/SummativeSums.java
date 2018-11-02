143310d03ebad9a480348c0aacfab300f027766c
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-02 16:46:15 -0500

    CHOWINDWRK-48 Summator

--- mileStone1/SummativeSums/src/main/java/SummativeSums.java
+++ mileStone1/SummativeSums/src/main/java/SummativeSums.java
@@ -10,6 +10,15 @@
  */
 public class SummativeSums {
     public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         
+        System.out.println("The sum of array1 = " + countArray(array1));
+    }
+    public static int countArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 }
