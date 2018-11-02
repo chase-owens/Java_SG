b588cd11fecbb45081a6912928cfd3955ae80f73
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-01 23:14:53 -0500

    CHOWINDWRK-42 fizzbuzz

--- TraditionalFizzBuzz/src/main/java/TraditionalFizzBuzz.java
+++ TraditionalFizzBuzz/src/main/java/TraditionalFizzBuzz.java
@@ -8,6 +8,29 @@
  *
  * @author chaseowens
  */
+import java.util.Scanner;
+
 public class TraditionalFizzBuzz {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many units fizzing and buzzing do you need in your life? ");
+        int number = input.nextInt();
+
+        for (int i = 0; i < number + 1; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+        System.out.println("TRADITION!!!!!");
+
+    }
 }
