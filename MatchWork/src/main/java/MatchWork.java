303ee0d42b30d60cd21f1b5fbae338cd44a68a97
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-03 09:55:42 -0500

    MatchWork

--- MatchWork/src/main/java/MatchWork.java
+++ MatchWork/src/main/java/MatchWork.java
@@ -13,15 +13,15 @@ public class MatchWork {
         System.out.println(" The word Cart should come before Horse alphabetically : " + comesBefore("cart", "horse"));
         System.out.println(" Half of 42 = " + halfOf(42));
         System.out.println(" (short) Pi = " + pi());
-        System.out.println(" This first letter of the word Llama is: " + firstLetter(Llama));
+        System.out.println(" This first letter of the word Llama is: " + firstLetter("Llama"));//????????
         System.out.println(" 1337 x 1337 = " + times1337(1337));
     }
-    public static int times1337(int n) {
-        return 1337 * n;
-    }
-    
     public static String firstLetter(String word) {
         return word.substring(0, 1);
+    } 
+    
+    public static int times1337(int n) {
+        return 1337 * n;
     }
     
     public static double pi() {
@@ -31,7 +31,7 @@ public class MatchWork {
     public static double halfOf(double num) {
         return (num / 2);
     }
-    public static boolean comeaBefore(String a, String b) {
+    public static boolean comesBefore(String a, String b) {
         return a.compareToIgnoreCase(b) < 0;
     }
 }
