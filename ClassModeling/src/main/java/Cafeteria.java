774ecdbc8ca4170533b765cb2f9e2bb8cb247fd6
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-05 16:46:39 -0600

    SchoolModel

---
+++ ClassModeling/src/main/java/Cafeteria.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chaseowens
+ */
+class Cafeteria {
+    private int numberOfStoves;
+    private int seatingCapacity;
+    private double squareFoot;
+    private int numberOfServingLines;
+    private int mealsServedPerHour;
+
+    public int getNumberOfStoves() {
+        return numberOfStoves;
+    }
+
+    public void setNumberOfStoves(int numberOfStoves) {
+        this.numberOfStoves = numberOfStoves;
+    }
+
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(int seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public double getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(double squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getNumberOfServingLines() {
+        return numberOfServingLines;
+    }
+
+    public void setNumberOfServingLines(int numberOfServingLines) {
+        this.numberOfServingLines = numberOfServingLines;
+    }
+
+    public int getMealsServedPerHour() {
+        return mealsServedPerHour;
+    }
+
+    public void setMealsServedPerHour(int mealsServedPerHour) {
+        this.mealsServedPerHour = mealsServedPerHour;
+    }
+}
---
+++ ClassModeling/src/main/java/School2.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chaseowens
+ */
+public class School2 {
+    
+    private int enrollment;
+    private int numberOfTeachers;
+    private String[] coursesOffered;
+    private String mascot;
+    private String[] clubsOffered;
+    private Student[] roster;
+    
+    public void enrollStudent(Student student) {
+    }
+    
+    public void unenrollStudent(Student student) {
+    }
+
+    public int getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(int enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public int getNumberOfTeachers() {
+        return numberOfTeachers;
+    }
+
+    public void setNumberOfTeachers(int numberOfTeachers) {
+        this.numberOfTeachers = numberOfTeachers;
+    }
+
+    public String[] getCoursesOffered() {
+        return coursesOffered;
+    }
+
+    public void setCoursesOffered(String[] coursesOffered) {
+        this.coursesOffered = coursesOffered;
+    }
+
+    public String getMascot() {
+        return mascot;
+    }
+
+    public void setMascot(String mascot) {
+        this.mascot = mascot;
+    }
+
+    public String[] getClubsOffered() {
+        return clubsOffered;
+    }
+
+    public void setClubsOffered(String[] clubsOffered) {
+        this.clubsOffered = clubsOffered;
+    }
+
+    public Student[] getRoster() {
+        return roster;
+    }
+
+    public void setRoster(Student[] roster) {
+        this.roster = roster;
+    }
+    
+}
