015793e18aefe3e78e06f8a0aa3a930714845bb1
Chase-Owens <chasejonathanowens@gmail.com>
2018-11-05 14:27:03 -0600

    CHOWINDWRK-49 HiddenNuts

--- HiddenNuts/src/main/java/HiddenNuts.java
+++ HiddenNuts/src/main/java/HiddenNuts.java
@@ -8,6 +8,22 @@
  *
  * @author chaseowens
  */
+import java.util.Random;
+
 public class HiddenNuts {
-    
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        
+        String[] hidingSpots = new String[100];
+        Random squirrel = new Random();
+        
+        hidingSpots[squirrel.nextInt(hidingSpots.length)] = "Nut";
+        System.out.println("The nut has been hidden...");
+        
+        for (int i = 0; i < hidingSpots.length; i++) {
+            if (hidingSpots[i].equals("Nut")) {
+                System.out.println("Found it! The nut is at index " + i);
+            }
+        }
+    }
 }
